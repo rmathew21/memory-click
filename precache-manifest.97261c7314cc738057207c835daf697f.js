ca0b42591c06675b7e784e7c4d97e0ca4fa06646
rob <robmathew21@gmail.com>
2019-07-11 17:38:13 -0500

    Updates

---
+++ precache-manifest.97261c7314cc738057207c835daf697f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37171553a5b3ce6dc2009f2ab1893f4a",
+    "url": "/memory-click/index.html"
+  },
+  {
+    "revision": "98025600d229f3d1d4e8",
+    "url": "/memory-click/static/css/main.4f45f314.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/memory-click/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "98025600d229f3d1d4e8",
+    "url": "/memory-click/static/js/main.c406126d.chunk.js"
+  },
+  {
+    "revision": "d590ecd6a3c92c2b7d8b",
+    "url": "/memory-click/static/js/runtime~main.99b5961d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memory-click/static/media/logo.5d5d9eef.svg"
+  }
+]);
