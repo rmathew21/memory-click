e4fac08ee8decf9a6a06f3c14cba6183b6d269a4
rob <robmathew21@gmail.com>
2019-07-10 19:39:40 -0500

    added shoes.json, added shuffle function to app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Jcard from './components/Jcard';
+import Wrapper from "./components/Wrapper";
 import logo from './logo.svg';
 import './App.css';
+import shoes from "./shoes.json";
 
 
 class App extends Component {
   state = {
     message: "Click a Jordan to start",
     score: 0,
-    highScore: 0
-  }
-}
+    highScore: 0,
+    shoes
+  };
 
-function App() {
+  // Shuffle shoes 
+   shuffle = a => {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  };
+
+  // identify the shoe that was clicked
+  handleClick = id => {
+    // Filter this.state.shoes for shoes with an id not equal to the id being removed
+    const shoes = this.state.shoes.filter(shoe => shoe.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ shoes });
+  };
+
+
+render() {
   return (
     <div className="App">
+      <nav class="navbar bg-primary">
+        Clicky Game!!
+      </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -31,6 +54,8 @@ function App() {
       </header>
     </div>
   );
+}
+  
 }
 
 export default App;
--- src/components/Jcard.js
+++ src/components/Jcard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Jcard = props => (
     <div className="card">
         <div className="img-container">
-            <a onClick={() => props.selectJ(props.name)}
+            <a onClick={() => props.shuffleShoes(props.name)}
                 className={props.score === 0 ? "style-click" : "style-click" }
                 >
                 <img alt={props.name} src={props.image} />
