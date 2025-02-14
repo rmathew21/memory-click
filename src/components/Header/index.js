7816d4d732ad8d01bb286bc3e7a4edeb877dbf43
rob <robmathew21@gmail.com>
2025-02-14 16:24:48 -0600

    updated header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,8 @@ import "./style.css";
 function Header() {
   return (
     <header className="header">
-      <h1>Clicky Game!</h1>
-      <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+      <h1>Click a Jordan Game!</h1>
+      <h2>Click on a shoe to earn points, but don't click on the same shoe twice!</h2>
     </header>
   );
 }
