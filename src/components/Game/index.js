2379136c0e1441d8b1061dee719410ca85615e39
rob <robmathew21@gmail.com>
2025-02-17 00:26:27 -0600

    removed extra slash from all imports in game

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import Nav from "../Nav";
 import Header from "../Header";
-import Container from "../Container/";
-import ClickItem from "../ClickItem/";
-import Footer from "../Footer/";
+import Container from "../Container";
+import ClickItem from "../ClickItem";
+import Footer from "../Footer";
 import data from "../../data.json";
 
 class Game extends Component {
